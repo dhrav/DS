952a333c673fe2aef0c1d6eb8270b2acb2ec889e
B <sindhub@informatica.com>
2018-07-14 20:07:51 +0530

    Structural check is refactored

--- BinaryTree/FoldableBT.java
+++ BinaryTree/FoldableBT.java
@@ -2,11 +2,11 @@ package BinaryTree;
 
 public class FoldableBT {
     public static void main(String[] args) {
-        /*Node root1 = new Node(10);
+        Node root1 = new Node(10);
         root1.left = new Node(7);
         root1.right = new Node(15);
         root1.left.right = new Node(9);
-        root1.right.left = new Node(11);*/
+        root1.right.left = new Node(11);
 
 /*        Node root1 = new Node(10);
         root1.left = new Node(7);
@@ -20,48 +20,31 @@ public class FoldableBT {
         root1.left.left = new Node(5);
         root1.right.left = new Node(11);*/
 
-        Node root1 = new Node(10);
+     /*   Node root1 = new Node(10);
         root1.left = new Node(7);
         root1.right = new Node(15);
         root1.left.left = new Node(9);
         root1.left.right = new Node(10);
-        root1.right.left = new Node(12);
+        root1.right.left = new Node(12);*/
 
-
-
-        if(root1 == null || (root1.left == null && root1.right == null)) {
-            System.out.println(true);
-        }
-        else {
-            System.out.println(isFoldable(root1.left, root1.right));
-        }
+        System.out.println(isFoldable(root1));
     }
 
-    private static boolean isSingleNode(Node root) {
-        return root.left == null && root.right == null;
+    private static boolean isFoldable(Node root) {
+        return isStructureSame(root, root);
     }
 
-    private static boolean isFoldable(Node root1, Node root2) {
+    private static boolean isStructureSame(Node root1, Node root2) {
+
         if(root1 == null && root2 == null) {
             return true;
         }
-        else if((root1 == null && root2 != null) || (root1 != null && root2 == null)) {
-            return false;
+
+        if(root1 != null && root2 != null) {
+            return isStructureSame(root1.left, root2.right) && isStructureSame(root1.right, root2.left);
         }
 
-        if(isSingleNode(root1) && isSingleNode(root2)) {
-            return true;
-        }
-
-        boolean result;
-        if((root1.left == null && root2.right != null) || (root1.right == null && root2.left != null))  {
-            return false;
-        }
-        else {
-            result = isFoldable(root1.left, root2.right) && isFoldable(root1.right, root2.left);
-
-        }
-        return result;
+        return false;
     }
 
 }
